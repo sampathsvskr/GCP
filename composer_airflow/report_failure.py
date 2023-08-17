04abb0107de7659718976066611e896704e5e9d3
Sampath Kumar Reddy Soma <sampathkumarreddysoma@Sampaths-MacBook-Air.local>
2023-08-17 16:36:48 +0530

    updated

--- composer_airflow/report_failure.py
+++ composer_airflow/report_failure.py
@@ -10,3 +10,10 @@ def report_failure(context):
     html_content = "job failure"
     to_emails = ["sampath@gmail.com"]
     send_email_smtp(to_emails, subject, html_content)
+
+
+
+def report_failure_1(context):
+    
+    send_email = EmailOperator(task_id = "failure_email", to = ["sampath@gmail.com"], subject = "job failure", html_content = "job failure", dag = dag)
+    send_email.execute(context)

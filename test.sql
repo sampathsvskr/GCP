c13548a210583e29fafd3d1497e5d9ecc0bd474b
sampathsvskr <37656675+sampathsvskr@users.noreply.github.com>
2024-11-26 23:07:40 +0530

    Update test.sql

--- test.sql
+++ test.sql
@@ -1,1 +1,15 @@
 
+
+SELECT 
+  vin,
+  df_partition_date as date, 
+  veh_model, 
+  model_year as year, 
+  monitor_timestamp as timestamp,
+  hit_count_1 as hitcount1,
+  hit_count_2 as hitcount2, 
+  step_in_flag as stepin_flg,
+  
+FROM 
+    `{}.bq_34_tcu4g_feature_fdp_dwc_vw.ncvdc62_fnv2_psa_vw`
+

a96635252b17aa13f5cf92478236b31e67413403
sampathsvskr <37656675+sampathsvskr@users.noreply.github.com>
2024-11-26 23:09:16 +0530

    Update test.sql

--- test.sql
+++ test.sql
@@ -8,7 +8,7 @@ SELECT
   monitor_timestamp as timestamp,
   hit_count_1 as hitcount1,
   hit_count_2 as hitcount2, 
-  step_in_flag as stepin_flg,
+  step_in_flag as stepin_flg
   
 FROM 
      wam.wam_pivot

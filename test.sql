350fc0abfde44799c40b5d7035b5f3f423cd2a45
sampathsvskr <37656675+sampathsvskr@users.noreply.github.com>
2024-11-27 00:11:01 +0530

    Update test.sql

--- test.sql
+++ test.sql
@@ -1,15 +1,1 @@
 
-
-SELECT 
-  vin,
-  df_partition_date as date, 
-  veh_model, 
-  model_year as year, 
-  monitor_timestamp as timestamp,
-  hit_count_1 as hitcount1,
-  hit_count_2 as hitcount2, 
-  step_in_flag as stepin_flg
-  
-FROM 
-     wam.wam_pivot
-

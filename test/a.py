d8ea7a82f538eb03c214a00a92c20fdf6608b3cd
sampathsvskr <37656675+sampathsvskr@users.noreply.github.com>
2024-12-10 21:21:09 +0530

    Delete test/a.py

--- test/a.py
+++
@@ -1,3 +0,0 @@
-files_active = [ os.path.join(dirpath, filename).replace(resource_directory,'') 
-    for dirpath, dirnames, filenames in os.walk(resource_directory) for filename in filenames]
-files_active = [item for item in files_active if search_string in item and 'cloudpsql' not in item]
